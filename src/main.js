09d3ad56e61235684b08e9e6b539e06fea3c3b5d
filippogenesini <78311187+filippogenesini@users.noreply.github.com>
2021-02-17 22:46:18 +0100

    cose

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
-;(() => {
-  console.log("prova")
+;(function () {
+
+  var menu = document.getElementById("js-menu")
+  var tendina = document.getElementById("js-tendina")
+  var testoMenuAperto = "Chiudi"
+  var testoMenuChiuso = "Menu"
+
+  var dropdown = function () {
+    console.log("ho cliccato")
+    tendina.classList.toggle("nascosto")
+    
+    if (menu.text == testoMenuChiuso) {
+      menu.text = testoMenuAperto
+    } else {
+      menu.text = testoMenuChiuso
+    }
+  }
+
+
+  menu.addEventListener("click", dropdown)
+
+
+
 })()
